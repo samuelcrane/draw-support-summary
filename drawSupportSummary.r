e6b4e2093a938247818d4240c00c7ede6974afd3
Samuel Crane <sam@samuelcrane.com>
2013-07-07 20:07:40 -0400

    Update drawSupportSummary.r
    Just cleaning things up a bit.

--- drawSupportSummary.r
+++ drawSupportSummary.r
@@ -34,20 +34,22 @@ getAllSubtrees <- function(phy, minSize=2)
 
 # The plotSupportSummary function will add a grey-filled circle on the maximum likelihood tree if at 
 # least one of the ML, parsimony, or Bayesian trees supports that node and will draw a black-filled
-# circle if all three do. Parts of this come from label-node-support and parts from draw-node-support-symbol 
+# circle if all three do. The subtree comparison was inspired by Rich Glor and the dot plotting was
+# inspired by Emmanuel Paradis's R book. Both have been adapted and extended here. 
 plotSupportSummary <- function(targetTree, suppTree1, suppTree2) 
 {
-    targetTree <- ladderize(root(targetTree, "Outgroup_species"))
+    targetTree <- ladderize(root(targetTree, "Outgroup_species")) # A visual manipulation of the RAxML output. Not necessary. 
     getAllSubtrees(targetTree) -> targetSub
     getAllSubtrees(suppTree1) -> suppSub1
     getAllSubtrees(suppTree2) -> suppSub2
     suppList1 <- matrix("", Nnode(targetTree), 1)
     suppList2 <- matrix("", Nnode(targetTree), 1)
     
-    #The commands below compare all the subclades in the Bayes tree to all the subclades in the bootstrap tree, and vice versa, and identifies all those clades that are identical.
+    #The commands below compare all the subclades in the targetTree tree to all the subclades in the other trees, and vice versa, and identifies 
+    # all those clades that are identical. One for loop for each tree comparison with 
     for(i in 1:Nnode(targetTree)) 
     {
-        for(j in 1:Nnode(suppTree1)) 
+        for(j in 1:Nnode(suppTree1)) # suppTree1 is the bayesTree in this example
         {
             match(targetSub[[i]]$tip.label[order(targetSub[[i]]$tip.label)], suppSub1[[j]]$tip.label[order(suppSub1[[j]]$tip.label)]) -> shared
             match(suppSub1[[j]]$tip.label[order(suppSub1[[j]]$tip.label)], targetSub[[i]]$tip.label[order(targetSub[[i]]$tip.label)]) -> shared2
@@ -56,7 +58,7 @@ plotSupportSummary <- function(targetTree, suppTree1, suppTree2)
                 round(as.numeric(suppTree1$node.label[j]), digits=2) -> suppList1[i]
             }
         }
-        for(j in 1:Nnode(suppTree2)) 
+        for(j in 1:Nnode(suppTree2)) # suppTree2 is the mpTree in this example
         {
             match(targetSub[[i]]$tip.label[order(targetSub[[i]]$tip.label)], suppSub2[[j]]$tip.label[order(suppSub2[[j]]$tip.label)]) -> shared
             match(suppSub2[[j]]$tip.label[order(suppSub2[[j]]$tip.label)], targetSub[[i]]$tip.label[order(targetSub[[i]]$tip.label)]) -> shared2
@@ -65,17 +67,23 @@ plotSupportSummary <- function(targetTree, suppTree1, suppTree2)
                 suppTree2$node.label[j] -> suppList2[i]
             }
         }
-    }
+    } 
     plot(targetTree, cex=0.5, lwd=0.5, direction='r', use.edge.length=FALSE, label.offset=1, no.margin=TRUE, x.lim=c(0.0000, 300), y.lim=c(0, 110)) 
 
-    # Color palette for filled circles
+    # Color palette for filled circles. Change these to change the color of the circles. 
     co <- c("black", "grey", "white")
 
-    # Initilize character matrix for drawing node circles
+    # Initilize character matrix for drawing node circles. Each matrix element corresponds to one node, in order of the targetTree. 
     p <- character(length(targetTree$node.label))
-    p[] <- co[3] # Set all nodes to white
-    p[(as.numeric(targetTree$node.label) >= 70) | (suppList1 >= 0.90) | (suppList2 >= 70)] <- co[2] # If at least one tree provides good support, set node to grey
-    p[(as.numeric(targetTree$node.label) >= 70) & (suppList1 >= 0.90) & (suppList2 >= 70)] <- co[1] # If all three trees provide good support, set node to black
+    
+    # Set all nodes to white
+    p[] <- co[3] 
+    
+    # If at least one tree provides good support, set node to grey. Change values here to change the "good support" threshold. 
+    p[(as.numeric(targetTree$node.label) >= 70) | (suppList1 >= 0.90) | (suppList2 >= 70)] <- co[2]
+    
+    # If all three trees provide good support, set node to black. These values should really be the same as above. Or not. 
+    p[(as.numeric(targetTree$node.label) >= 70) & (suppList1 >= 0.90) & (suppList2 >= 70)] <- co[1] 
     
     # This for loop draws filled circles on only those nodes with good support from at least one method. 
     for(j in 1:Nnode(targetTree)) 
